f9c52057c8d1de2d7e6c3c50f4438f1f2c0a49de
sharannarang <sharan.narang@gmail.com>
2014-09-17 10:56:11 -0400

    Modifications to helper.R

--- helper.R
+++ helper.R
@@ -1,4 +1,5 @@
-epl <- read.csv("E0.csv")
+## 
+epl <- read.csv("epl_13_14.csv")
 
 ## Convert Team Names to factor variables
 epl$HomeTeam <- as.factor(epl$HomeTeam)
@@ -10,6 +11,21 @@ epl <- epl[,1:23]
 ## Remove division and date of game played
 epl <- epl[,3:23]
 
-## Melt the data frame
-epl.melt <- melt(epl, id.vars = c("HomeTeam", "AwayTeam"), measure.vars = colnames(epl)[c(3,4,6,7,10:21)])
+## Get referee data
+referee <- epl[,9]
 
+## Get points data from HTR/FTR columns
+epl$HTFTP <- ifelse(epl$FTR == 'H', 3, ifelse(epl$FTR == 'D', 1, 0))
+epl$ATFTP <- ifelse(epl$FTR == 'A', 3, ifelse(epl$FTR == 'D', 1, 0))
+epl$HTHTP <- ifelse(epl$HTR == 'H', 3, ifelse(epl$HTR == 'D', 1, 0))
+epl$ATHTP <- ifelse(epl$HTR == 'A', 3, ifelse(epl$HTR == 'D', 1, 0))
+
+## Remove referee, result information from the main data frame
+epl <- epl[,-c(5,8,9)]
+
+## Melt the data frame
+epl.melt <- melt(epl, id.vars = c("HomeTeam", "AwayTeam"))
+
+## Create Home/Away casts
+epl.home.cast <- dcast(epl.melt, AwayTeam ~ variable, sum)
+epl.home.cast <- dcast(epl.melt, HomeTeam ~ variable, sum)
